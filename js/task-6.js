3eb905ec6247e01c50956d1b5375318e7c027de1
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-01-14 00:20:32 +0200

    solve task-6

--- js/task-6.js
+++ js/task-6.js
@@ -4,9 +4,54 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const input = document.querySelector("input");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createButton.addEventListener("click", () => {
+  const amount = Number(input.value);
+
+  if (amount < 1 || amount > 100 || !Number.isInteger(amount)) {
+    return;
+  }
+
+  createBoxes(amount);
+  input.value = "";
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = "";
+
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
 const link = document.createElement("link");
 link.rel = "stylesheet";
 link.href =
   "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap";
 
 document.head.appendChild(link);
+
+createButton.style.backgroundColor = "#4e75ff";
+createButton.classList.add("create-button");
+destroyButton.style.backgroundColor = "#ff4e4e";
+destroyButton.classList.add("destroy-button");
